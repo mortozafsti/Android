02c23085105546efe5a86d13cba26bd48564617d
mortozafsti <mortozafsti@gmail.com>
2019-03-31 08:32:05 +0600

    java Update

--- First_App/app/src/main/java/com/example/firstapp/MainActivity.java
+++ First_App/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text1);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
