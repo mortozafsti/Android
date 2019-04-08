b43d25b1f12e3fa97a82362decdd8827b0c793f0
mortozafsti <mortozafsti@gmail.com>
2019-04-08 12:00:55 +0600

    java Update

--- Sqlite/app/src/main/java/com/example/sqlite/Message.java
+++ Sqlite/app/src/main/java/com/example/sqlite/Message.java
@@ -4,8 +4,9 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class Message {
-
     public static void message(Context context,String message) {
         Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+
+
     }
 }
